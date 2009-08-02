2aadcfe77e02b1cd0e80a04aedf0c0acae3ed079
Navya <jammulan@rockhopper.cs.pdx.edu>
2009-08-02 16:44:55 -0700

    Added License Information to pages - Navya - 8/2

--- DoctorsPage.aspx
+++ DoctorsPage.aspx
@@ -1,25 +1,68 @@
+<%-- 
+  Copyright (c) 2009 Navya Jammula.
+
+  This file is part of HealthMonitoringSystem.
+  HealthMonitoringSystem is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+  
+  HealthMonitoringSystem is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with HealthMonitoringSystem.  If not, see
+  <http://www.gnu.org/licenses/>.
+
+--%>
+
 <%@ Page Language="C#" Inherits="HealthMonitorSystem.DoctorsPage" MasterPageFile="~/HealthMaster.master" %>
 
 <asp:Content id="doctors" ContentPlaceHolderID="cphMain" runat="server">
     <div>
-    <asp:GridView AutoGenerateColumns="false" 
+    <asp:GridView AutoGenerateColumns="false" Width="90%" HorizontalAlign="Center" 
     runat="server"
     ID="gv" 
-    OnRowCommand="gv_RowEditing" 
+    OnRowCommand="gv_RowCommand" 
     >
     <Columns>
-    <asp:BoundField DataField="ID" HeaderText="ID" />
     <asp:BoundField DataField="Name" HeaderText="Name"   />
     <asp:BoundField DataField="dob" DataFormatString="{0:MM-dd-yyyy}" HtmlEncode="false" HeaderText="Date of Birth"  />
     <asp:BoundField DataField="address" HeaderText="Address" />
+    <asp:BoundField DataField="doctorremarks" HeaderText="Remarks" />    
     <asp:TemplateField HeaderText="View History">
     <ItemTemplate>
-    <asp:LinkButton ID="bthView" runat="server" CommandName="View" CommandArgument=<%# DataBinder.Eval(Container.DataItem, "ID") %> >View</asp:LinkButton>
+    <asp:LinkButton ID="btnView" runat="server" CommandName="View" CommandArgument=
+    		<%# DataBinder.Eval(Container.DataItem, "ID") %> >View</asp:LinkButton>
+    </ItemTemplate>
+    </asp:TemplateField>
+    <asp:TemplateField HeaderText="Add Comments">
+    <ItemTemplate>
+    <asp:LinkButton ID="btnRemarks" runat="server" CommandName="Remarks" CommandArgument=
+    		<%# DataBinder.Eval(Container.DataItem, "ID") %> >Add</asp:LinkButton>
     </ItemTemplate>
     </asp:TemplateField>
     </Columns>
     </asp:GridView>
     </div>
     <br />
+    <asp:PlaceHolder id="phRemarks" runat="server">
+    <table>
+    <tr>
+    <td>
+    	Remarks &nbsp;&nbsp;&nbsp;&nbsp;
+    	<asp:TextBox id="txtRemarks" runat="server" Rows="5" Columns="75" TextMode="MultiLine" />
+    </td>
+    </tr>
+    <tr>
+    <td align="center"> 
+    	<asp:Button id="btnRemarks" runat="server" Text="Save" OnClick="btnRemarks_Click" />
+    	<asp:Button id="btnCancel" runat="server" Text="Cancel" OnClick="btnCancel_Click" />
+    </td>
+    </tr>
+    </table>
+    </asp:PlaceHolder>
     
 </asp:Content>
--- DoctorsPage.aspx.cs
+++ DoctorsPage.aspx.cs
@@ -1,4 +1,21 @@
+//-----------------------------------------------------------------------
+// Copyright (c) 2009 Navya Jammula.
 
+// This file is part of HealthMonitoringSystem.
+// HealthMonitoringSystem is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// HealthMonitoringSystem is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with HealthMonitoringSystem.  If not, see
+// <http://www.gnu.org/licenses/>.
+//-----------------------------------------------------------------------
 using System;
 using System.Web;
 using System.Web.UI;
@@ -10,6 +27,22 @@ namespace HealthMonitorSystem
 	public partial class DoctorsPage : BasePage
 	{
 		private static string _strSort;
+		
+		private int SelectedPId
+		{
+			get 
+			{
+				if(ViewState["SelectedPId"] != null)
+				{
+					return 	Convert.ToInt32(ViewState["SelectedPId"]);
+				}
+				return 0;
+			}
+			set 
+			{
+				ViewState["SelectedPId"] = value;
+			}
+		}
 
     protected void Page_Load(object sender, EventArgs e)
     {
@@ -18,6 +51,10 @@ namespace HealthMonitorSystem
 			this.ErrorMessage = "Please login to the System";
 			Response.Redirect("Default.aspx", true);
 		}
+			if(!IsPostBack)
+			{
+				phRemarks.Visible = false;
+			}
     }
 
     protected override void OnPreRender(EventArgs e)
@@ -33,8 +70,17 @@ namespace HealthMonitorSystem
             	dv.Sort = _strSort;
             	dt = dv.ToTable();
         	}
-        	gv.DataSource = dt;
-        	gv.DataBind(); 					
+			if(dt.Rows.Count > 0)
+			{
+        		gv.DataSource = dt;
+        		gv.DataBind(); 					
+			}
+			else
+			{
+				this.ErrorMessage = "Sorry you donot have any patients Assigned. <br />" +
+							"You can go on a vacation.";
+						
+			}
 		}
 		}catch(Exception ex)
 		{
@@ -45,7 +91,7 @@ namespace HealthMonitorSystem
     private DataTable RefreshData()
     {
         string strSql = " select dr.*, l.*, l.lastname ||  ' ' || l.firstname as name" +
-						" from doctorremarks dr " +
+						" from doctorpatient dr " +
 						" inner join login l on dr.patientid=l.id " +
 						" where dr.doctorid = " + this.UserId;
         DataSet ds = BaseDA.ExecuteDataSet(strSql);
@@ -56,13 +102,24 @@ namespace HealthMonitorSystem
 		return null;
     }
 
-    protected void gv_RowEditing(object sender, GridViewCommandEventArgs e)
+    protected void gv_RowCommand(object sender, GridViewCommandEventArgs e)
     {
 			if(e.CommandName == "View")
 			{
 				Response.Redirect("PatientHist.aspx?id="+e.CommandArgument , true);											
 			}
+			else if(e.CommandName == "Remarks")
+			{
+				this.SelectedPId = Convert.ToInt32(e.CommandArgument);
+				this.ShowRemarks();
+			}
     }
+		
+	private void ShowRemarks()
+	{
+		phRemarks.Visible = true;
+		txtRemarks.Text = string.Empty;
+	}
 
     protected void gv_Sorting(object sender, GridViewSortEventArgs e)
     {
@@ -72,5 +129,36 @@ namespace HealthMonitorSystem
             _strSort += " " + "DESC";
         }
     }
-	}
+
+    protected void btnCancel_Click (object sender, System.EventArgs e)
+    {
+		phRemarks.Visible = false;	
+    }
+
+    protected void btnRemarks_Click (object sender, System.EventArgs e)
+    {	
+		try
+		{
+			if(string.IsNullOrEmpty(txtRemarks.Text.Trim()))
+			{
+				this.ErrorMessage += "Remarks is Required <br />";
+			}
+			else
+			{
+				//Save Remarks
+				string strSql = string.Format("Update doctorpatient set doctorremarks='{0}' where " +
+								"doctorid = {1} and patientid = {2} ", 
+				                 BaseDA.Escape(txtRemarks.Text.Trim()),
+				                 this.UserId, this.SelectedPId);
+				BaseDA.ExecuteNonQuery(strSql);
+				phRemarks.Visible = false;
+				this.SelectedPId = 0;
+			}
+		}
+		catch(Exception ex)
+		{
+			this.ErrorMessage = ex.Message;
+		}
+    }
+}
 }
